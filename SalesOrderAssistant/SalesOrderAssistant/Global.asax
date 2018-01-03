188be75d0ae354921a38564e28ccb2bd55a1dc1b
Daniel <danielbrogers594@gmail.com>
2018-01-03 13:33:03 -0500

    first commit

---
+++ SalesOrderAssistant/SalesOrderAssistant/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SalesOrderAssistant.MvcApplication" Language="C#" %>
---
+++ SalesOrderAssistant/SalesOrderAssistant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalesOrderAssistant.Startup))]
+namespace SalesOrderAssistant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
